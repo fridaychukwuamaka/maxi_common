234effa65a6e226e7ae579366b9cccf424415e72
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2022-09-11 21:25:13 +0100

    update

--- lib/themes/app_style.dart
+++ lib/themes/app_style.dart
@@ -3,10 +3,22 @@ part of 'themes.dart';
 class AppStyle {
   AppStyle._();
 
-    static const double appPaddingVal = 16;
+  static const double appPaddingVal = 16;
 
   static const dialogBorderRadius = BorderRadius.only(
     topLeft: Radius.circular(AppStyle.appPaddingVal),
     topRight: Radius.circular(AppStyle.appPaddingVal),
+  );
+
+  static const modalDecoration = BoxDecoration(
+    borderRadius: AppStyle.dialogBorderRadius,
+    color: Colors.white,
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black12,
+        blurRadius: 10,
+        offset: Offset(0, 6),
+      ),
+    ],
   );
 }
